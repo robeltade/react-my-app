9b223d1545d09c3480f5a0882e6c2fa899783ae9
robel tadesse <robeltadesse37@gmail.com>
2023-12-23 08:05:58 +1200

    header of portifolio created

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+//import cv
+/* <a href={CV} download className='btn'> Download CV</a> */
+
+
+function CTA() {
+  return (
+    <div className="cta">
+       
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA

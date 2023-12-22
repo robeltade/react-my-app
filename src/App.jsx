f64b4fa4601a3ad59a2652814a6477fa4b767ae5
robel tadesse <robeltadesse37@gmail.com>
2023-12-23 07:26:23 +1200

    setting up css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+//import Service from './components/services'
+import Portifolio from './components/portifolio/Porifolio'
+import Testimonial from './components/testimonial/Testimonial'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
-function App() {
+const App=()=> {
   return (
-    <div>robeltade</div>
-  )
-}
+    <>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    
+    <Portifolio/>
+    <Testimonial/>
+    <Contact/>
+    <Footer/>
+
+    </>
+  );
+};
 
 export default App
